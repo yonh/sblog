6c5099f484398aad7533e8a7cc1367d6b519425c
yonh <azssjli@163.com>
2015-05-12 00:35:15 +0800

    修复链接错误

--- app/app.rb
+++ app/app.rb
@@ -3,9 +3,9 @@ require 'sinatra'
 
 class App < Sinatra::Application
 	get '/' do
-	  	IO.read('../org/html/index.html')
+	  	IO.read('/opt/org/html/index.html')
 	end
 	get '/:id' do
-		IO.read("../org/html/#{params['id']}.html")
+		IO.read("/opt/org/html/#{params['id']}.html")
 	end
 end
